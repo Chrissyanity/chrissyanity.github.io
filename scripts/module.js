41fb3a858340be50a3fa6f23e9de7fbf09598936
Christina L <clee04@baker.edu>
2017-04-14 01:22:42 -0400

    favicon and page load issue

--- scripts/module.js
+++ scripts/module.js
@@ -8,7 +8,7 @@ app.config(function($routeProvider, $locationProvider) {
         })
 
         .otherwise({
-            redirectTo: '/'
+            redirectTo: '/homepage'
         });
     $locationProvider.hashPrefix('');
 
